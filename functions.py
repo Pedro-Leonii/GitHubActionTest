b5fd4ae7631d750e80d2faa34ace6ac5d47b6791
Pedro-Leonii <pedroleoni2001@gmail.com>
2024-09-26 20:35:20 +0200

    add comments to fibonacci

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
 
+# PRE: n is an integer  
+# POST: fibonacci serie result fro n 
+
 def fibonacci(n):
     if(n < 0):
         return 0
-        
+
     if(n == 0 or n == 1):
         return 1
     
